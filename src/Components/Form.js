7a6ac59193334f926f2dd346273f19db1980acdb
abhishek-codilar <abhishek.s@codilar.com>
2021-12-20 10:32:13 +0530

    strike out and empty todo error solved

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -37,7 +37,7 @@ export default function Form() {
     const addItem = (e)=> {
 
         e.preventDefault();
-        if(!inputData){
+        if(!inputData || inputData.trim().length == 0){
             alert('No TODO mentioned....');
         }
         else{
@@ -126,7 +126,7 @@ export default function Form() {
                             return(
                                 <li key={i} className="lists">
                                 <input type="checkbox" className="item-check" checked={el.completed}   onClick={() => completeHandler(el.id)}/>
-                                <span>{el.text}</span>  
+                                <span style={el.completed?{textDecoration:"line-through"}:null}>{el.text}</span>  
                                 <svg xmlns="http://www.w3.org/2000/svg"className="svg" width="18" height="18"onClick={() => del(i)}><path  d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"></path></svg>
                             </li>
                             )
